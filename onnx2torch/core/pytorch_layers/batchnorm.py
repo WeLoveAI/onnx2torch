5cff2af09c2a51530788d593875104a263a6e821
inisis <desmond.yao@buaa.edu.cn>
2024-02-18 18:51:16 +0800

    fix bn bug

--- onnx2torch/core/pytorch_layers/batchnorm.py
+++ onnx2torch/core/pytorch_layers/batchnorm.py
@@ -6,9 +6,15 @@ from torch.nn.parameter import Parameter
 class BatchNorm(nn.Module):
     @classmethod
     def from_onnx(cls, mod):
-        bn = nn.BatchNorm2d(mod.inputs[1].shape[0], eps=mod.attrs["epsilon"])
-        bn.scale = Parameter(torch.from_numpy(mod.inputs[1].values))
-        bn.bias = Parameter(torch.from_numpy(mod.inputs[2].values))
+        bn = nn.BatchNorm2d(
+            mod.inputs[1].shape[0],
+            eps=mod.attrs["epsilon"],
+            momentum=mod.attrs["momentum"],
+        )
+        bn.weight = Parameter(
+            torch.from_numpy(mod.inputs[1].values), requires_grad=False
+        )
+        bn.bias = Parameter(torch.from_numpy(mod.inputs[2].values), requires_grad=False)
         bn.running_mean = Parameter(
             torch.from_numpy(mod.inputs[3].values), requires_grad=False
         )
