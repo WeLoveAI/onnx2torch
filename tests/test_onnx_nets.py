fff0faed2dc64b9fc19a3155095b93c18cd8e7a4
inisis <desmond.yao@buaa.edu.cn>
2024-02-24 09:40:08 +0800

    add timm test

--- tests/test_onnx_nets.py
+++ tests/test_onnx_nets.py
@@ -2,6 +2,7 @@ import os
 import warnings
 
 import pytest
+import timm
 import torch
 import torchvision.models as models
 
@@ -24,9 +25,27 @@ class TestTorchVisionClass:
             models.googlenet,
         ),
     )
-    def test_torchvision(self, request, model, shape=(1, 3, 224, 224), fuse=FUSE):
+    def test_torchvision(self, request, model, shape=(1, 3, 224, 224)):
         model = model(pretrained=PRETRAINED)
         x = torch.rand(shape)
+        torch.onnx.export(model, x, "tmp/" + request.node.name + ".onnx")
+
+        convert(
+            "tmp/" + request.node.name + ".onnx",
+            "tmp/" + request.node.name + ".gm",
+            check=True,
+        )
+
+
+class TestTimmClass:
+    @pytest.fixture(params=timm.list_models())
+    def model_name(self, request):
+        yield request.param
+
+    def test_timm(self, request, model_name):
+        model = timm.create_model(model_name, pretrained=PRETRAINED)
+        input_size = model.default_cfg.get("input_size")
+        x = torch.randn((1,) + input_size)
         torch.onnx.export(model, x, "tmp/" + request.node.name + ".onnx")
 
         convert(
